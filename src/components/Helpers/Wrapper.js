17ce3a4545a8f76f1a1aae491a7f643365e280b2
dcat <36656965+angelaunique@users.noreply.github.com>
2022-04-20 22:42:46 -0700

    replace <div> with Wrapper to sol return problem

---
+++ src/components/Helpers/Wrapper.js
@@ -0,0 +1,5 @@
+const Wrapper = (props) => {
+  return props.children; // children prop holds all the content that passing bw the openning and closing tag of the custom component
+};
+export default Wrapper;
+//basically an empty compoent
